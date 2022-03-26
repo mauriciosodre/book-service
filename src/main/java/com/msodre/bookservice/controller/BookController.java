e18c1ecf22901ed818adcafb981014b42e9326dc
msodrejr <m.sodre8@gmail.com>
2022-03-26 14:23:46 -0300

    Add OpenApi documentation and configs

--- src/main/java/com/msodre/bookservice/controller/BookController.java
+++ src/main/java/com/msodre/bookservice/controller/BookController.java
@@ -3,6 +3,8 @@ package com.msodre.bookservice.controller;
 import com.msodre.bookservice.model.Book;
 import com.msodre.bookservice.integration.cambiointegration.CambioIntegration;
 import com.msodre.bookservice.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Book Endpoint")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/book-service")
@@ -20,6 +23,7 @@ public class BookController {
   private final BookRepository repository;
   private final CambioIntegration cambioIntegration;
 
+  @Operation(summary = "Find a specific book by your id")
   @GetMapping("/{id}/{currency}")
   public Book findBook(@PathVariable Long id, @PathVariable String currency) {
     var port = environment.getProperty("local.server.port");
--- src/main/java/com/msodre/bookservice/controller/FooBarController.java
+++ src/main/java/com/msodre/bookservice/controller/FooBarController.java
@@ -1,13 +1,14 @@
 package com.msodre.bookservice.controller;
 
 import io.github.resilience4j.bulkhead.annotation.Bulkhead;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Foo Bar Endpoint")
 @RestController
 @RequestMapping("book-service")
 public class FooBarController {
